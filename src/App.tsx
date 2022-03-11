827f3e9cf7b97588ffc28cdf75fd97e7beb00c82
Lauren Hale <lauren.m.hale7@gmail.com>
2022-03-10 21:48:03 -0600

    feat(set-up): configure webpack for images

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import './styles.css'
+import IMAGE from './cam.png'
 export const App = () => {
-    return <h1>My Project</h1>
+    return<>
+     <h1>My Project</h1>
+     <img src={IMAGE} alt="camera"/>
+    </>
+  
 }
