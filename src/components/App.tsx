536c0d93b7c8e61b338c982ac1761a510ebcae37
Lauren Hale <lauren.m.hale7@gmail.com>
2022-03-11 15:38:03 -0600

    feat(news-api): make interfaces and reducers

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import "../styles.css";
+
+import  NewsFeed  from "./NewsFeed";
+const App = () => {
+  return (
+    <>
+      <h1>News Fix</h1>
+      <NewsFeed/>
+    </>
+  );
+};
+
+export default App;
