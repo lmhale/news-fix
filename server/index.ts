0d06994ee6bafd938cb40e7485cc101df01f46ca
Lauren Hale <lauren.m.hale7@gmail.com>
2022-03-12 13:38:59 -0600

    express serve up and running

--- server/index.ts
+++ server/index.ts
@@ -1,24 +1,24 @@
 import { createConnection, getConnectionOptions } from "typeorm";
+import express from "express";
 import { User } from "./entities/User";
-import { Favorite } from "./entities/Favorite"
+import { Favorite } from "./entities/Favorite";
 
-
+const app = express();
 const main = async () => {
-    
   try {
     const connectionOptions = await getConnectionOptions();
     await createConnection({
-        ...connectionOptions,
-        entities:[
-            User,
-            Favorite
-        ]
+      ...connectionOptions,
+      entities: [User, Favorite],
     });
-    console.log("successfully connected to DB")
+    console.log("successfully connected to DB");
+    app.use(express.json());
+    app.listen(8080, () => {
+      console.log("Now running on port 8080");
+    });
   } catch (error) {
-    console.error(error)
-    throw new Error("unable to connect to database")
+    console.error(error);
+    throw new Error("unable to connect to database");
   }
-  
 };
 main();
