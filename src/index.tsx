a23f9ce37154dc201eec1ecccfe1c0b95ab8e885
Lauren Hale <lauren.m.hale7@gmail.com>
2022-03-12 09:04:40 -0600

    merge refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,3 @@
-import ReactDom from 'react-dom';
-import App from './components/App';
-import Store from '../src/context/dataStore'
-ReactDom.render(
-    <Store>
-    <App />
-    </Store>,
- document.getElementById('root')
-);
+import ReactDom from "react-dom";
+import App from "./components/App";
+ReactDom.render(<App />, document.getElementById("root"));
