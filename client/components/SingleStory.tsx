541f286484e3153b96227666b5fa92d5e198a7a0
Lauren Hale <lauren.m.hale7@gmail.com>
2022-03-14 16:35:02 -0500

    post to favorites works

--- client/components/SingleStory.tsx
+++ client/components/SingleStory.tsx
@@ -3,23 +3,26 @@ import { useSaveFavoriteMutation } from "../redux-app/features/favorites/favorit
 
 
 
-export const SingleStory = ({title, description, source, image, url}) => {
+export const SingleStory = ({title, description, source, image, url, publishedAt}) => {
 
     const [addNewFavorite, { isLoading }] = useSaveFavoriteMutation()
-    interface IFavoriteData {
-    title: "string";
-    description: "string";
-    source: "string";
-    image: "string";
-    url: "string";
-  }
+
+    //     interface IFavoriteData {
+//     title: "string";
+//     description: "string";
+//     source: "string";
+//     image: "string";
+//     url: "string";
+//   }
+    const [disable, setDisable] = React.useState(false);
 //   const [favorite, setFavorite ] = useState<IFavoriteData | undefined> (props)
 
   const userId = 'aca80fa9-d7c6-41c6-85bc-05ffda6e5f9d'
   
     const onSaveFavoriteClicked = async () => {
           try {
-            await addNewFavorite( {title, description, source, image, url, userId} ).unwrap()
+              setDisable(true)
+            await addNewFavorite( {title, description, source, image, url, publishedAt, userId} ).unwrap()
            
           } catch (err) {
             console.error('Failed to save the post: ', err)
@@ -35,7 +38,7 @@ export const SingleStory = ({title, description, source, image, url}) => {
           <img width="250px" src={image} />
         </div>
         <a href={url}>Read More...</a>
-        <button onClick={()=>onSaveFavoriteClicked()} style={{ margin: "10px" }}>
+        <button disabled={disable} onClick={()=>onSaveFavoriteClicked()} style={{ margin: "10px" }}>
           Save
         </button>
       </div>
