a01d6ece77171b252c768bb63bc55dd375020067
Lauren Hale <lauren.m.hale7@gmail.com>
2022-03-15 11:14:01 -0500

    set links with a tag for data refresh

--- client/components/NavBar.tsx
+++ client/components/NavBar.tsx
@@ -2,10 +2,12 @@ import React from "react"
 import {Link} from "react-router-dom"
 export const NavBar = () => {
     return (
-        <div>
-        
-        <Link style={{margin:"10px"}} to="/favorites">Favorites</Link>
-        <Link to="/">Home</Link>
+        <div >
+
+        <a style={{margin:"10px"}} href="/">Home</a>
+        <a href="favorites">Favorites</a>
+        <Link style={{margin:"10px"}}  to={"/login"} >Login</Link>
+
         </div>
     )
 }
